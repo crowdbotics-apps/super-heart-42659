a9e377d5d647f3522bcf1bd44bb7eb5c5630e6fd
Crowdbotics <team@crowdbotics.com>
2023-08-05 10:25:45 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "super-heart-42659",
+  url: "https://super-heart-42659.botics.co",
+  api: "https://super-heart-42659.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
